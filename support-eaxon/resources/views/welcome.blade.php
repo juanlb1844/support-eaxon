01d68622afeab8b203e02c7553f418a0bed7cc3f
juanLB <juanlb1844@gmail.com>
2022-05-14 17:16:47 -0500

    prueba demo de contenido

--- support-eaxon/resources/views/welcome.blade.php
+++ support-eaxon/resources/views/welcome.blade.php
@@ -1,25 +1,197 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>SUPPORT</title>
+    <title>Soporte | eAxón</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
+	<style type="text/css">
+		.np { padding: 0px!important; }
+		body, html { font-weight: 900; }
+		.general-content {
+			padding: 20px 10vw;
+		}
+		.title-page { font-weight: bolder; font-size: 25px;}
+
+		.content-slider {
+			background-image: url('{{asset('media/back.png')}}'); 
+			background-size: contain;
+			background-repeat: no-repeat;
+			background-position: center;
+			height: 70vh; 
+		}
+
+		.content-mge-slider {
+			padding-top: 170px; 
+			font-size: 75px; 
+		}
+
+
+		.section-questions { font-size: 25px; padding: 70px 0px; }
+		.list-group { font-size: 14px; font-weight: 600; }
+	</style>
+
 </head>
+
+
 <body>
 
-    <div class="container">
+    <div class="container-fluid general-content">
     	<div class="header-page">
     		<div class="title-page">
-    			<h1>Página de soporte</h1>
+    			<span>eAxón</span>
     		</div>
-    		<div class="body-page">
-    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    	</div>
+		<div class="body-page">
+			<div class="slider-main col-lg-12">
+				<div class="col-lg-4 content-mge-slider">
+					<div>
+						<span>Ayuda de eAxón</span>
+					</div>
+				</div>
+				<div class="col-lg-8 content-slider">
+					
+				</div>
+			</div>
+			<div class="section section-questions row">
+				<div class="col-lg-12">
+					<span>Buscar preguntas frecuentes</span>
+					<h2></h2>
+				</div>
+				<div class="col-lg-4">
+					<div class="list-group">
+					  <a href="#" class="list-group-item disabled">Cuentas</a>
+					  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+					  <a href="#" class="list-group-item">Morbi leo risus</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="list-group">
+					  <a href="#" class="list-group-item disabled">Cuentas</a>
+					  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+					  <a href="#" class="list-group-item">Morbi leo risus</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="list-group">
+					  <a href="#" class="list-group-item disabled">Cuentas</a>
+					  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+					  <a href="#" class="list-group-item">Morbi leo risus</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					  <a href="#" class="list-group-item">Vestibulum at eros</a>
+					  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+					</div>
+				</div>
+			</div>
+
+			<style type="text/css">
+				.section-community { text-align: center; }
+				.header-section {
+					font-size: 30px;
+				}
+				.content-section-community {
+					font-size: 20px; 
+					color: gray;
+					font-weight: 600;
+				}
+				.content-section-c {
+					width: 40vw;
+					display: inline-block;
+					text-align: left;
+				}
+			</style>
+
+			<div class="section section-community">
+				<div class="content-section-c">
+					<div class="header-section">
+						<span>Nuestro equipo está dispuesto a ayudarte y solucionar todas tus dudas</span>
+					</div>
+					<div class="content-section-community">
+						<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+					</div>
+					<div>
+						<a href=""><span>prueba nuestra demo ></span></a>
+					</div>
+				</div>
+			</div>
+
+		</div> 
+    </div>
+
+    <style type="text/css">
+    	.footer-content { min-height: 20vh; padding-top: 10vh; text-align: center;}
+    	.footer-content ul li {
+    		display: inline-block;
+    		font-weight: 600;
+    	}
+    </style>
+
+    <div class="footer container">
+    	<div class="footer-content row">
+    		<ul>
+    			<li>eAxón</li>
+    			<li>Guadalajara Jalisco, Andares. CP</li>
+    			<li>
+    				<a href="">
+    					Política de privacidad
+    				</a>
+    			</li>
+    			<li>
+    				<a href="">
+    					Términos y condiciones
+    				</a>
+    			</li>
+    			<li>
+    				<a href="">
+    					Configuración de cookies
+    				</a>
+    			</li>
+    			<li>
+    				<a href="">Sistema</a>
+    			</li>
+    		</ul>
+    		<div class="subtitle-footer">
+    			<span>Soliciones eAxón</span>
     		</div>
     	</div>
     </div>
+
+
+<script type="text/javascript">
+	$('.btn').click( function() {
+		alert(".."); 
+	}); 
+</script>
 
 </body>
 </html>
